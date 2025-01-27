a7a95c91708d93e5b6c8fbc095f052593edc9c60
ibheelz <ibheelz@icloud.com>
2025-01-27 22:31:05 +0100

    Passed a KeyError

--- main.py
+++ main.py
@@ -221,15 +221,39 @@
 
 ### DAY 30 ###
 
-fruits = ["Apple", "Pear", "Orange"]
+# fruits = ["Apple", "Pear", "Orange"]
+#
+# # Catch the exception and make sure the code runs without crashing.
+# def make_pie(index):
+#     try:
+#         fruit = fruits[index]
+#         print(fruit + " pie")
+#     except IndexError:
+#         print("Fruit pie")
+#
+# make_pie(4)
 
-# Catch the exception and make sure the code runs without crashing.
-def make_pie(index):
-    try:
-        fruit = fruits[index]
-        print(fruit + " pie")
-    except IndexError:
-        print("Fruit pie")
+facebook_posts = [
+    {'Likes': 21, 'Comments': 2},
+    {'Likes': 13, 'Comments': 2, 'Shares': 1},
+    {'Likes': 33, 'Comments': 8, 'Shares': 3},
+    {'Comments': 4, 'Shares': 2},
+    {'Comments': 1, 'Shares': 1},
+    {'Likes': 19, 'Comments': 3}
+]
 
-make_pie(4)
+def count_likes(posts):
+    total_likes = 0
+    for post in posts:
+        try:
+            total_likes += post['Likes']
+        except KeyError:
+            # If 'Likes' is missing, just skip this post
+            pass
+    return total_likes
+
+# Call the function
+print(count_likes(facebook_posts))
+
+
 
